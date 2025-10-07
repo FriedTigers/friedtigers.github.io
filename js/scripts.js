44f78d8a4fb192cfa64d07bdd909918bb29f1bda
FriedTigers <91367367+FriedTigers@users.noreply.github.com>
2025-10-08 00:06:26 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -61,7 +61,7 @@ function kakaoShare() {
     Kakao.Link.createScrapButton({
         container: '#kakao-link-btn',
         requestUrl: 'https://friedtigers.github.io/',
-    templateId: ${124938}
+        templateId: ${124938}
     });
 
     // 아래 부분은 주석 처리하여 실행되지 않게 함
